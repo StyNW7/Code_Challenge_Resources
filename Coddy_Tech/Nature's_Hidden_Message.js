801663c463370353ee0b267fa6c4ba7bd61bd97d
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-05-03 16:49:32 +0700

    Update Nature's_Hidden_Message.js

--- Coddy_Tech/Nature's_Hidden_Message.js
+++ Coddy_Tech/Nature's_Hidden_Message.js
@@ -1,3 +1,5 @@
+// Link: https://coddy.tech/challenges/natures_hidden_message_js
+
 function decodeNatureMessage(stoneSequence, skipPattern) {
     let s = "";
     for (let i = 0; i < stoneSequence.length; i += skipPattern){

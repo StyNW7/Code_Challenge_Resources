0a520570e87598a77383f37ec864889d4ed41c9b
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-05-09 10:20:00 +0700

    Create maximumHappinessSum.cpp

---
+++ LeetCode/Medium/maximumHappinessSum.cpp
@@ -0,0 +1,17 @@
+Link: https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort (happiness.begin(), happiness.end(), greater<int>());
+        int i = 0;
+        long long ans = 0;
+
+        while (k--){
+            happiness[i] = max (happiness[i]-i, 0);
+            ans += happiness[i++];
+        }
+
+        return ans;
+    }
+};

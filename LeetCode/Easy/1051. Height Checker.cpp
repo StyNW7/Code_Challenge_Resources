6ee83f62cca1f1f557c61efd3d7330c69569a2f3
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-06-10 09:56:30 +0700

    Create 1051. Height Checker.cpp

---
+++ LeetCode/Easy/1051. Height Checker.cpp
@@ -0,0 +1,14 @@
+// Link: https://leetcode.com/problems/height-checker/
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector <int> v(heights);
+        sort (v.begin(), v.end());
+        int count = 0;
+        for (int i = 0; i < heights.size(); i++){
+            if (heights[i] != v[i]) count++;
+        }
+        return count;
+    }
+};

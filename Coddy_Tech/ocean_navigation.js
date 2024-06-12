d7669d6600c019a4b50cf1c7248c95b185f473fa
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-06-12 21:37:20 +0700

    Create ocean_navigation.js

---
+++ Coddy_Tech/ocean_navigation.js
@@ -0,0 +1,5 @@
+// Link: https://coddy.tech/challenges/ocean_navigation_js
+
+function navigateOcean(currentPosition, destination) {
+    return currentPosition[0] === destination[0] || currentPosition[1] === destination[1] ? true : false
+}

320e90891c61f6ca2050527cf77bd4dbee15a0c1
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-06-21 16:24:54 +0700

    Create act_of_valor.js

---
+++ Coddy_Tech/act_of_valor.js
@@ -0,0 +1,8 @@
+// Link: https://coddy.tech/challenges/act_of_valor_js
+
+function actOfValor(action) {
+    if (action === 'save a life') return "This is an act of valor!"
+    else if (action === 'help the elderly') return "This is an act of valor!"
+    else if (action === 'defend the weak') return "This is an act of valor!"
+    else return "This action is not considered an act of valor."
+}

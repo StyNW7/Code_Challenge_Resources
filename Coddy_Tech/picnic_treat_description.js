c0ee3e96093084cc0a5c8c0e01ece93469353ee5
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-07-28 10:33:29 +0700

    Create picnic_treat_description.js

---
+++ Coddy_Tech/picnic_treat_description.js
@@ -0,0 +1,5 @@
+// Link: https://coddy.tech/challenges/picnic_treat_description_js
+
+function describePicnicTreat(food, drink) {
+    return "The squirrel is eyeing your delicious " + food + " and refreshing " + drink + "!"
+}

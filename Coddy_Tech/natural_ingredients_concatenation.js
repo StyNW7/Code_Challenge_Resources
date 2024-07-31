4b1d285af56f99412fe1085fce09e0b3dba9fb6b
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-07-31 18:15:26 +0700

    Create natural_ingredients_concatenation.js

---
+++ Coddy_Tech/natural_ingredients_concatenation.js
@@ -0,0 +1,6 @@
+// Link: https://coddy.tech/challenges/natural_ingredients_concatenation_js
+
+function combineIngredients(ingredients, separator = ", ") {
+    // Concatenate all ingredients using the specified or default separator
+    return ingredients.join(separator);
+}

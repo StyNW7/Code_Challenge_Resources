4a12a2a1ab9182b2fbc29643e36fab6b05172b0d
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-08-05 19:53:13 +0700

    Create wilderness_signpost_message.js

---
+++ Coddy_Tech/wilderness_signpost_message.js
@@ -0,0 +1,7 @@
+// Link: https://coddy.tech/challenges/wilderness_signpost_message_js
+
+function createSignpostMessage(distance, landmark) {
+    // Construct the message using template literals
+    const message = `Follow the trail for ${distance} miles to the ${landmark}.`;
+    return message;
+}

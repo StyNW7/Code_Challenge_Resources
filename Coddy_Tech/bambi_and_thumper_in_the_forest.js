66883fd39018286afb5f9e452a7a518858251d62
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-08-20 09:52:54 +0700

    Create bambi_and_thumper_in_the_forest.js

---
+++ Coddy_Tech/bambi_and_thumper_in_the_forest.js
@@ -0,0 +1,8 @@
+// Link: https://coddy.tech/challenges/bambi_and_thumper_in_the_forest_js?daily=true
+
+function deerObserver(deerName, observerName) {
+    if (deerName === 'Bambi' && observerName === 'Thumper'){
+        return "Hello, Bambi! Thumper is happy to see you today in the peaceful forest."
+    }
+    else return "The observer didn\'t spot Bambi today, the forest is still peaceful."
+}

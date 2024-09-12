2b966e3bbceb9bc4c588bcca2b741cceb19b7512
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-09-12 09:12:03 +0700

    Create 1684-count-the-number-of-consistent-strings.cpp

---
+++ LeetCode/Easy/1684-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int arr[26] = {0};
+        for (char c : allowed){
+            arr[c-'a'] = 1;
+        }
+
+        int count = 0;
+
+        for (string word : words){
+            int flag = 1;
+            for (char c : word){
+                if (arr[c-'a'] == 0){
+                    flag = 0;
+                    break;
+                }
+            }
+            count += flag;
+        }
+
+        return count;
+
+    }
+};

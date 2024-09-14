2df1187e294a3c9e03cbf0a88dcf0c39ceacf6e2
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-09-14 09:27:03 +0700

    Create lilys_pony_lineup.js

---
+++ Coddy_Tech/lilys_pony_lineup.js
@@ -0,0 +1,7 @@
+// Link: https://coddy.tech/challenges/lilys_pony_lineup_js?daily=true
+
+function arrangePonies(initialPonies, poniesToAdd) {
+    const finalPoniesArrangement = initialPonies.concat(poniesToAdd);
+    
+    return finalPoniesArrangement;
+}

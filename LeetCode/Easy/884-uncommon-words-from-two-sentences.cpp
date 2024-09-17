5d632969fd9eefcbc4cf5a0c9448e7a53090b046
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-09-17 07:16:06 +0700

    Create 884-uncommon-words-from-two-sentences.cpp

---
+++ LeetCode/Easy/884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,28 @@
+// Link: https://leetcode.com/problems/uncommon-words-from-two-sentences/
+
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string, int> wordCount;
+        vector<string> result;
+
+        auto countWords = [&](string sentence) {
+            stringstream ss(sentence);
+            string word;
+            while (ss >> word) {
+                wordCount[word]++;
+            }
+        };
+
+        countWords(s1);
+        countWords(s2);
+
+        for (const auto& entry : wordCount) {
+            if (entry.second == 1) {
+                result.push_back(entry.first);
+            }
+        }
+
+        return result;
+    }
+};
